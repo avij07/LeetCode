5d08df8dd867561fc3c01369211f54574fc5c0d9
Avinash Jaiswal <122218770+avij07@users.noreply.github.com>
2024-08-30 14:35:56 +0530

    Time: 3 ms (50.11%), Space: 45.9 MB (14.3%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int maxInd = 0;
+        for(int i = 0; i<= n-1; i++){
+            if (i>maxInd) return false;
+            maxInd = Math.max(maxInd, i+nums[i]);
+        }
+        return true;
+        
+    }
+}
